619f128993bb7ee4d6262bbecea6bc415b4c3d3f
niliak <ongkailin@hotmail.com>
2024-01-27 02:59:37 +0800

    up till solution finishing our restaurant card

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -12,12 +12,11 @@ const SafeArea = styled(SafeAreaView)`
 
 const RestaurantListContainer = styled.View`
   flex: 1;
-  padding: 16px;
-  background-color: blue;
+  padding: ${(props) => props.theme.space[3]};
 `;
 
 const SearchContainer = styled.View`
-  padding: 16px;
+  padding: ${(props) => props.theme.space[3]};
 `;
 
 export const RestaurantsScreen = () => (
